a093594cfd2dd908dcd8b844717d1222b0f51546
Franklin Crosby <obleo33@gmail.com>
2016-12-06 07:56:30 -0700

    Event listner and if statement added to JS

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
-var userInput = document.getElementById('userInput');
+//buttons
 var guessButton = document.getElementById('guessButton');
+var resetButton = document.getElementById('resetButton');
+var clearButton = document.getElementById('clearButton');
+
+// input
+var userInput = document.getElementById('userInput');
+
+// global variables
 var startNum = 1;
 var endNum = 100;
-var targetNumber = Math.floor((Math.random() * endNum) + startNum);
+var targetNum = Math.floor((Math.random() * endNum) + startNum);
+var userNum = "";
+
+
+guessButton.addEventListner('click',function(){
+  userNum = parseInt(userInput.value);
+});
+
+if (userNum === targetNum){
+
+}else if (userNum<targetNum) {
+
+}else {
+  
+}
+
 
 console.log(targetNumber);
-console.log(userChoice)
-
-guessButton.addEventListner('click',function())
+console.log(userNum);

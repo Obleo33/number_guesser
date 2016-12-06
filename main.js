2eec80423786c6e38575cf63f92ab21629ac31dd
Franklin Crosby <obleo33@gmail.com>
2016-12-06 07:38:18 -0700

    JS number guesser started

---
+++ main.js
@@ -0,0 +1,10 @@
+var userInput = document.getElementById('userInput');
+var guessButton = document.getElementById('guessButton');
+var startNum = 1;
+var endNum = 100;
+var targetNumber = Math.floor((Math.random() * endNum) + startNum);
+
+console.log(targetNumber);
+console.log(userChoice)
+
+guessButton.addEventListner('click',function())

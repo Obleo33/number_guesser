a4e456b0e6fb27339c3b7ead8840b6708dc9c85f
Franklin Crosby <obleo33@gmail.com>
2016-12-07 12:42:08 -0700

    Add check if input is NaN, too high or too low

--- main.js
+++ main.js
@@ -2,53 +2,39 @@
 var guessButton = document.getElementById('guessButton');
 var resetButton = document.getElementById('resetButton');
 var clearButton = document.getElementById('clearButton');
-
 // input
 var userInput = document.getElementById('userInput');
-
 // global variables
 var startNum = 1;
 var endNum = 100;
 var targetNum = Math.floor((Math.random() * endNum) + startNum);
 var userNum = "";
-var lastGuess = document.querySelector('.lastGuess');
-var guessTitle = document.querySelector('.guessTitle');
-var guessDir = document.querySelector('.guessDir');
 
-function minmax(value, min, max){
-  if(value < min || isNaN(parseInt(value)))
-    return min;
 
-  else if(value > max)
-    return max;
-  else
-    return value;
-}
-
+// helper functions
 var message = function (message,number,result){
+  var lastGuess = document.querySelector('.lastGuess');
+  var guessTitle = document.querySelector('.guessTitle');
+  var guessDir = document.querySelector('.guessDir');
   guessTitle.innerText = message;
   lastGuess.innerText = number;
   guessDir.innerText = result;
+};
+
+var minmax = function(value, min, max){
+
+  if(isNaN(value) === true){
+    message("You must enter a number","","");
+  }else if(value < min){
+    message("the minimum number is "+ startNum,"","choose a number " + startNum + " to " + endNum);
+  }else if(value > max){
+    message("the maximum number is " + endNum,"", "choose a number " + startNum + " to " + endNum);
+  }else{
+    submit(userNum);
+  }
 }
 
-message("Guess a number",userNum,startNum + " to " + endNum)
-
-guessButton, resetButton, clearButton
-
-console.log(targetNum);
-
-// if (userInput.value ===""){
-//  guessButton.disabled = true;
-// }
-// else{
-//   guessButton.disabled = false;
-// }
-
-
-guessButton.addEventListener('click',function(){
-  userNum = parseInt(userInput.value);
-  userNum = minmax(userNum,startNum,endNum);
-  userInput.value = "";
+var submit = function (userNum){
   if (userNum === targetNum){
     message("You are correct",userNum,"BOOM!");
   }else if (userNum<targetNum) {
@@ -56,21 +42,24 @@ guessButton.addEventListener('click',function(){
   }else {
     message("Your last guess was",userNum, "That is too high");
   }
+}
+
+message("Guess a number",userNum,startNum + " to " + endNum);
+
+guessButton.addEventListener('click',function(){
+  userNum = parseInt(userInput.value);
+  minmax(userNum,startNum,endNum);
+  userInput.value = "";
 });
 
 resetButton.addEventListener('click',function(){
   targetNum = Math.floor((Math.random() * endNum) + startNum);
-  message("Guess a number",userNum, startNum + " to " + endNum);
-  lastGuess.innerText = "";
+  message("Guess a number","", startNum + " to " + endNum);
   console.log(targetNum);
 });
 
 clearButton.addEventListener('click', function(){
   userInput.value = "";
-})
+});
 
-
-
-
-
-// disable buttons
+console.log(targetNum);

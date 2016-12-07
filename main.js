29a48b6b746b2120a9a06e8af8fa32e51bb6def5
Franklin Crosby <obleo33@gmail.com>
2016-12-07 07:57:37 -0700

    Update css and addclear function to guess button

--- main.js
+++ main.js
@@ -22,9 +22,14 @@ var message = function (message,result){
   guessDir.innerText = result;
 }
 
+message("Guess a number",startNum + " to " + endNum)
+
+console.log(targetNum);
+
 guessButton.addEventListener('click',function(){
   userNum = parseInt(userInput.value);
   console.log(userNum);
+  userInput.value = "";
   if (userNum === targetNum){
     message("You are correct","BOOM!");
   }else if (userNum<targetNum) {
@@ -35,8 +40,17 @@ guessButton.addEventListener('click',function(){
 });
 
 resetButton.addEventListener('click',function(){
-  
+  targetNum = Math.floor((Math.random() * endNum) + startNum);
+  message("Guess a number",startNum + " to " + endNum);
+  userNum = ""
+  console.log(targetNum);
+});
+
+clearButton.addEventListener('click', function(){
+
 })
+
+
 
 
 
